9632e5e20a448d114e8a4192deee6677680302e3
Nicolas Derumigny <nderumigny@gmail.com>
2022-08-12 17:43:16 +0200

    Compatibility with VS2022

--- init-bench-ue5.bat
+++ init-bench-ue5.bat
@@ -3,8 +3,8 @@
 :: Cloning repo
 PortableGit\bin\git.exe clone --depth 1 --branch 5.0.3-release https://github.com/EpicGames/UnrealEngine.git
 
-:: Unreal Engine internal cloning mechanism
 cd UnrealEngine
+:: Unreal Engine internal cloning mechanism
 call Setup.bat
 call GenerateProjectFiles.bat
 cd ..
